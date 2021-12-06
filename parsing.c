4ae70fcca7816996bf9d9f654846b6db3bfe18d9
Javier Pozuelo ruiz <jpozuelo@c3r4s3.42madrid.com>
2021-12-06 18:43:50 +0100

    casa

--- parsing.c
+++ parsing.c
@@ -1,15 +1,58 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   parsing.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jpozuelo <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/12/06 15:16:59 by jpozuelo          #+#    #+#             */
+/*   Updated: 2021/12/06 18:43:05 by jpozuelo         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "push_swap.h"
 
-static int	isNumber(char c);
+static int	is_sign_number(char c);
+static int	is_space(char c);
 
 static int isNumber(char c)
 {
 	return (c >= '0' && c <= '9');
 }
 
+static int	is_sign(char c)
+{
+	if (c == '-')
+		return (-1);
+	else
+		return (1);
+}
+
 int		ft_check_format(char *cadena)
 {
-	int sign;
+	static char	*dup;
+	static int	i = 0;
+	int			res;
 
+	if (cadena)
+	{
+		dup = ft_strdup(cadena);
+		i = 0;
+	}
+	while (dup[i] == ' ')
+	{
+		i++;
+	}
+	res = ft_atoi_c(dup + i, &i);
+	return (res);
+}
+
+int	ft_atoi_c(char *cadena, int *i)
+{
+	unsigned int	res;
+	int				sign;
+	register int	j;
+
+	j = *i;
 
 }
--- push_swap.h
+++ push_swap.h
@@ -1,34 +1,54 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   push_swap.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jpozuelo <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/12/03 18:41:08 by jpozuelo          #+#    #+#             */
+/*   Updated: 2021/12/06 16:27:16 by jpozuelo         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #ifndef PUSH_SWAP_H
 # define PUSH_SWAP_H
+# include <stdlib.h>
 
 typedef struct	s_node_s
 {
-	char		order;
-	int			content;
-	s_node_s	*next;
+	char				order;
+	int					content;
+	struct s_node_s		*next;
+	struct s_node_s		*previous;
 } t_node_s;
 
  typedef struct s_circular
 {
-	s_node_s	*first;
-	s_node_s	*last;
+	t_node_s	*first;
+	t_node_s	*last;
 } t_circular;
 
+//Utils
+char			*ft_strdup(char *cadena);
+
 //Funciones para la entrada de datos
-int			ft_atoi_c(char *cadena);
+int				ft_check_format(char *cadena);
+int				ft_atoi_c(char *cadena, int *i);
 
 //Operaciones de la lista circular
-void		add_first(t_circular list, t_node_s element);
-void		add_last(t_circular list, t_node_s element);
-void		clear(t_circular list);
+void			add_first(t_circular list, t_node_s element);
+void			add_last(t_circular list, t_node_s element);
+void			clear(t_circular list);
 
 //Operaciones disponibles para ordenar.
-void		swap(t_circular stack, char c);
-void		swap_s(t_circular stack_a, t_circular stack_b);
-void		push(t_circular stack, char c);
-void		rotate(t_circular list, char c);
-void		rotate_r(t_circular stack_a, t_circular stack_b);
-void		reverse_rotate(t_circular stack, char c);
-void		reverse_rotate_r(t_circular stack_a, t_circular stack_b);
+void			swap(t_circular stack, char c);
+void			swap_s(t_circular stack_a, t_circular stack_b);
+void			push(t_circular stack, char c);
+void			rotate(t_circular list, char c);
+void			rotate_r(t_circular stack_a, t_circular stack_b);
+void			reverse_rotate(t_circular stack, char c);
+void			reverse_rotate_r(t_circular stack_a, t_circular stack_b);
+
+//Funciones para implementar el checker
 
 #endif
