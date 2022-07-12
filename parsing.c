c03d04a8c6011dd8c5fd3c0440722aa461e859ab
Javier Pozuelo ruiz <jpozuelo@c1r10s5.42madrid.com>
2022-07-12 21:22:48 +0200

    3

--- parsing.c
+++ parsing.c
@@ -6,12 +6,41 @@
 /*   By: jpozuelo <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/06 15:16:59 by jpozuelo          #+#    #+#             */
-/*   Updated: 2022/07/12 20:28:57 by jpozuelo         ###   ########.fr       */
+/*   Updated: 2022/07/12 21:21:53 by jpozuelo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-int	init(t_monitor *mon)
+int	init(t_monitor *mon, int argc, char **argv)
 {
+	int		i;
+	char	*line;
+
+	i = 1;
+	mon->nlist = (int *) malloc(sizeof(int) * 1000);
+	while (i < argc)
+	{
+		line = argv[i++];
+		parser(mon, line);
+		if (mon->error == 1)
+			break;
+	}
+	return (mon->error);
+}
+
+void	parser(t_monitor *mon, char *line)
+{
+	char	c;
+
+	c = *line;
+	while (c != 0 && mon->error == 1)
+	{
+		if (c == '-')
+			n_atoi(++line);
+		else if (isNumber(c))
+			p_atoi(line);
+		else if (c == ' ')
+			line++;
+	}
 }
--- utils.c
+++ utils.c
@@ -6,7 +6,7 @@
 /*   By: jpozuelo <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/06 15:02:59 by jpozuelo          #+#    #+#             */
-/*   Updated: 2022/07/12 20:29:09 by jpozuelo         ###   ########.fr       */
+/*   Updated: 2022/07/12 21:21:52 by jpozuelo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ int		isNumber(char c)
 	return (c >= '0' && c <= '9')
 }
 
-int		p_atoi(char *str, )
+void	p_atoi(char *str, t_monitor *mon)
 {
 	int	res;
 	int	aux;
@@ -28,12 +28,16 @@ int		p_atoi(char *str, )
 		aux = res;
 		res = res * 10 + (*str - '0');
 		if (!isNumber(*str) || aux > res)
-			error_msg();
+		{
+			mon->error = 1;
+			break;
+		}
 	}
-	return (res);
+	(mon->list)[mon->amount] = res;
+	mom->amount++
 }
 
-int		n_atoi(char *str, t_monitor *mon)
+void	n_atoi(char *str, t_monitor *mon)
 {
 	int	res;
 	int	aux;
@@ -44,7 +48,11 @@ int		n_atoi(char *str, t_monitor *mon)
 		aux = res;
 		res = res * 10 - (*str - '0');
 		if (!isNumber(*str) || aux > res)
+		{
 			mon->error =  1;
+			break;
+		}
 	}
-	return (res);
+	(mon->list)[mon->amount] = res;
+	mon->amount++;
 }
